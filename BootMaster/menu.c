04d9f582dfd4714e56d33de8e0fa8f6a87b45f73
Abz79 <aterro@gmail.com>
2025-06-18 15:43:02 +0300

    Added right button clicks as alternative input and fixed drawing of selection when back from submenus also fixed a missed click when back from submenus

--- BootMaster/menu.c
+++ BootMaster/menu.c
@@ -2452,7 +2452,7 @@ UINTN DrawMenuScreen (
             if (StyleFunc != MainMenuStyle && pdGetState().Press) {
                 // Prevent user from getting stuck on submenus
                 // Only 'About' screen currently reachable without keyboard
-                MenuExit = MENU_EXIT_ENTER;
+                MenuExit = MENU_EXIT_ESCAPE;
                 break;
             }
         }
@@ -4137,15 +4137,16 @@ UINTN RunMainMenu (
 
     // Remove any buffered key strokes
     BREAD_CRUMB(L"%a:  4", __func__);
-    KeyStrokeFound = ReadAllKeyStrokes();
+//  KeyStrokeFound = ReadAllKeyStrokes();
 
     BREAD_CRUMB(L"%a:  5", __func__);
-    if (!KeyStrokeFound || !AppleFirmware) {
+    if (!AppleFirmware) {
+//    if (!KeyStrokeFound || !AppleFirmware) {
         BREAD_CRUMB(L"%a:  5a 1", __func__);
         if (!AppleFirmware) {
             // Always reset the buffer on UEFI PC
             BREAD_CRUMB(L"%a:  5a 1a 1", __func__);
-            REFIT_CALL_2_WRAPPER(gST->ConIn->Reset, gST->ConIn, FALSE);
+//          REFIT_CALL_2_WRAPPER(gST->ConIn->Reset, gST->ConIn, FALSE);
         }
         BREAD_CRUMB(L"%a:  5a 2", __func__);
     }
@@ -4160,7 +4161,8 @@ UINTN RunMainMenu (
         Style          = GraphicsMenuStyle;
         MainStyle      = MainMenuStyle;
         PointerEnabled = PointerActive = pdAvailable();
-        DrawSelection  = !PointerEnabled;
+        if (Screen->TimeoutSeconds > 0) {DrawSelection = !PointerEnabled;}
+        else {DrawSelection = TRUE;}
     }
 
     // Generate WaitList if not already generated.
