102dde97c75fd77099aa890b0dfad6543ae01901
aterro <aterro@yahoo.com>
2026-04-22 13:33:47 +0300

    cleanup

--- include/version.h
+++ include/version.h
@@ -9,12 +9,12 @@
  * source code or binaries made from it.
  *
  */
-/**
- ** Modified for RefindPlus
- ** Copyright (c) 2020-2025 Dayo Akanji (sf.net/u/dakanji/profile)
- **
- ** Modifications distributed under the preceding terms.
- **/
+/*
+ * Modified for RefindPlus
+ * Copyright (c) 2020-2026 Dayo Akanji (sf.net/u/dakanji/profile)
+ *
+ * Modifications distributed under the preceding terms.
+ */
 
 #define WIDE_STR2(x) L##x
 #define WIDE_STR(x) WIDE_STR2(x)
